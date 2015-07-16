8401ee921f9cb7a0995ae7f1083ac61b8d38c31c
Taavi Sangel <taavi@stagnationlab.com>
2015-07-16 18:05:49 +0300

    added nesting

--- gulpfile.js
+++ gulpfile.js
@@ -2,33 +2,43 @@
 
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
 
-var rootPath = './app/';
-var gfxPath = rootPath + 'gfx/';
+// post css
+var postcss = require('gulp-postcss');
+var mqpacker = require('css-mqpacker');
+var csswring = require('csswring');
+var cssnext = require('cssnext');
+var nested = require('postcss-nested');
 
 // Static Server + file watcher
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['css'], function() {
 	browserSync.init({
-		server: "./"
+		server: './'
 	});
 
-	gulp.watch(gfxPath + 'scss/*.scss', ['sass']);
+	gulp.watch('./app/gfx/css/**/*.css', ['css']);
 	gulp.watch('./app/**/*.html').on('change', browserSync.reload);
-	gulp.watch('./proto_engine/**.js').on('change', browserSync.reload);
+	//gulp.watch('./proto_engine/**.js').on('change', browserSync.reload);
 });
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-	return gulp.src(gfxPath + 'scss/*.scss')
+gulp.task('css', function () {
+	var processors = [
+		mqpacker,
+		csswring,
+		cssnext(),
+		nested
+	];
+	return gulp.src('./app/gfx/css/*.css')
 		.pipe(sourcemaps.init())
-		.pipe(sass())
-		.pipe(autoprefixer('last 2 version'))
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest(gfxPath + 'css/'))
-		.pipe(browserSync.stream());
+		.pipe(postcss(processors))
+		.on('error', function (err) {
+			console.error('Error!', err.message);
+			this.emit('end');
+		})
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('./app/gen'))
+		.pipe(browserSync.stream({match: '**/*.css'}));
 });
 
 gulp.task('default', ['serve']);
